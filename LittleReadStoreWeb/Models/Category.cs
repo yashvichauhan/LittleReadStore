d40ee0db4cf76cf8409760a3ea568868e65c864c
yashvichauhan <yashvichauhan19@gmail.com>
2025-05-02 23:04:59 -0400

    adding category updates

--- LittleReadStoreWeb/Models/Category.cs
+++ LittleReadStoreWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace LittleReadStoreWeb.Models
 {
@@ -8,7 +9,12 @@ namespace LittleReadStoreWeb.Models
         public int CategoryId { get; set; }
 
         [Required]
+        [StringLength(50, ErrorMessage = "Category Name cannot be longer than 50 characters!")]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+
+        [Range(1, 100,ErrorMessage ="Display Order must be between 1 and 100!")]
+        [DisplayName("Display Order")]
         public string DisplayOrder { get; set; }
     }
 }
