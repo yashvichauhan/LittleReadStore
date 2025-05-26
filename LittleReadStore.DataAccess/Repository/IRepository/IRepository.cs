440a2c23c86797f38fc39f3dc758667c8a0e8835
yashvichauhan <yashvichauhan19@gmail.com>
2025-05-26 16:06:19 -0400

    adding repository files

---
+++ LittleReadStore.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleReadStore.DataAccess.Repository.IRepository
+{
+    interface IRepository<T> where T: class
+    {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
